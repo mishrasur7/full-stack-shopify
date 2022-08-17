13439d9a3dfb7c222a876b4ee9fd99aad421ba3d
Suraj Mishra <mishrasur7@gmail.com>
2022-08-17 23:10:24 +0300

    Product model completed

---
+++ api/src/models/Product.ts
@@ -0,0 +1,55 @@
+import mongoose, { Document, ObjectId, Schema } from 'mongoose'
+
+export interface ProductDocument extends Document {
+  //productId: ObjectId,
+  title: string
+  price: number
+  sellerId: ObjectId
+  images: string[]
+  categories: ObjectId[]
+  reviews: ObjectId[]
+}
+
+const ProductDocumentSchema = new Schema({
+  /*  productId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        auto: true
+    }, */
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  sellerId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  categories: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+    },
+  ],
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'ProductReview',
+    },
+  ],
+})
+
+const Product = mongoose.model<ProductDocument>(
+  'Product',
+  ProductDocumentSchema
+)
+export default Product
