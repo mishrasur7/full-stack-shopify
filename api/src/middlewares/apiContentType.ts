b9ae96913ffaf550217306a3a098d42de2d507c5
Suraj Mishra <mishrasur7@gmail.com>
2022-08-26 00:02:39 +0300

    application/json requirement removed

--- api/src/middlewares/apiContentType.ts
+++ api/src/middlewares/apiContentType.ts
@@ -5,7 +5,7 @@ import { BadRequestError } from '../helpers/apiError'
 export default function (req: Request, res: Response, next: NextFunction) {
   if (
     (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') &&
-    !req.is('application/json')
+    req.is('application/json')
   ) {
     next(new BadRequestError('Request body must be of type json'))
   } else {
