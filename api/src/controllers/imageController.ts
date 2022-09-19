c7ffb9b0b49afbb451d29d3e6e998afed56da8e9
Suraj Mishra <mishrasur7@gmail.com>
2022-09-19 23:15:26 +0300

    functions added into imagecontroller

---
+++ api/src/controllers/imageController.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express'
+
+import { BadRequestError } from '../helpers/apiError'
+import imageService from '../services/imageServices'
+
+const findAll = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send(await imageService.getAllImages())
+  } catch (error){
+    if (error instanceof Error && error.name == 'ValidationError') {
+        next(new BadRequestError('Invalid Request', error))
+      } else {
+        next(error)
+      }
+  }
+}
+
+const findOne = async (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+const createOne = async (req: Request, res: Response, next: NextFunction) => {
+    
+}
+
+export default {
+    findAll,
+    findOne, 
+    createOne
+}
